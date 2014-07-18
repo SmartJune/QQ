c39471d6313266e0253de0cdd1cbad9fd9c70023
June <553763552@qq.com>
2014-07-18 11:22:16 +0800

    add basic socket part to support chatting

--- LocalNetworkChatting/src/ChatFrame.java
+++ LocalNetworkChatting/src/ChatFrame.java
@@ -4,12 +4,19 @@ import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.JFrame;
 
 
 public class ChatFrame extends JFrame{
-	
+	DataOutputStream dataOut = null;
+	Socket socket = null;
 	TextField textField = new TextField();
 	TextArea textArea = new TextArea();
 	
@@ -20,17 +27,56 @@ public class ChatFrame extends JFrame{
 		add(textField,BorderLayout.SOUTH);
 		add(textArea,BorderLayout.NORTH);
 		pack();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new WindowAdapter(){
+			public void WindowClosing(WindowEvent arg0){
+				disconnected();
+				System.exit(0);
+			}
+		});
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		textField.addActionListener(new TextFieldListener());
 		setVisible(true);
+		connectToServer();
+	}
+	public void connectToServer(){
+		try {
+			socket = new Socket("192.168.1.103",8887);
+			dataOut = new DataOutputStream(socket.getOutputStream());
+			
+			System.out.println("connect to server successfully.");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void disconnected(){
+		try {
+			dataOut.close();
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	private class TextFieldListener implements ActionListener{
 	@Override
 		public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-			String s = textField.getText().trim();
-			textArea.setText(s);
+			String string = textField.getText().trim();
+			textArea.setText(string);
 			textField.setText("");
+			
+			try {
+				dataOut.writeUTF(string);
+				dataOut.flush();
+		//		dataOut.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}	
 	}
 }
--- LocalNetworkChatting/src/ChatServer.java
+++ LocalNetworkChatting/src/ChatServer.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -5,11 +6,21 @@ import java.net.Socket;
 
 public class ChatServer {
 	public static void main(String[] args){
+		boolean started = false;
 		try {
-			ServerSocket ss = new ServerSocket(8888);
-			while(true){
+			ServerSocket ss = new ServerSocket(8887);
+			started = true;
+			while(started){
+				boolean connected = false;
 				Socket s = ss.accept();
-				System.out.println("connected success");
+				System.out.println("client connected success");
+				connected = true;
+				DataInputStream dataIn = new DataInputStream(s.getInputStream());
+				while(connected){
+					String string = dataIn.readUTF();
+					System.out.println(string);
+				}
+				dataIn.close();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
