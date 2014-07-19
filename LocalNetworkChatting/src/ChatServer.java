6b23afc3e7a3371f091a8f4d481c778b1d182c70
June <553763552@qq.com>
2014-07-19 10:26:26 +0800

    change into multi-thread typeto support more than one client

--- LocalNetworkChatting/src/ChatServer.java
+++ LocalNetworkChatting/src/ChatServer.java
@@ -5,42 +5,81 @@ import java.net.Socket;
 
 
 public class ChatServer {
+	boolean started = false;
+	ServerSocket ss = null;
+	
 	public static void main(String[] args){
-		boolean started = false;
-		ServerSocket ss = null;
-		Socket s = null;
-		DataInputStream dataIn = null;
 		
+	//	DataInputStream dataIn = null;
+		new ChatServer().start();
+	}
+	
+	public void start(){
 		try {
 			ss = new ServerSocket(8887);
+			started = true;
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 		try {
-			started = true;
 			while(started){
-				boolean connected = false;
-				s = ss.accept();
+			//	boolean connected = false;
+				Socket s = ss.accept();
+				Client c = new Client(s);
 				System.out.println("client connected success");
-				connected = true;
-				dataIn = new DataInputStream(s.getInputStream());
-				while(connected){
-					String string = dataIn.readUTF();
-					System.out.println(string);
+				Thread d = new Thread(c);
+				d.start();
 				}
-				dataIn.close();
-			}
+			//	dataIn.close();
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			
+		} finally{
 			try {
-				dataIn.close();
-				s.close();
-			} catch (IOException e1) {
+				ss.close();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
-			e.printStackTrace();
 		}
+	}
+	class Client implements Runnable{
+		private Socket s;
+		DataInputStream dataIn = null;
+		boolean connected = false;
+		
+		public Client(Socket s){
+			this.s = s;
+			try {
+				dataIn = new DataInputStream(s.getInputStream());
+				connected = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try{
+			while(connected){
+				String string = dataIn.readUTF();
+				System.out.println(string);
+			}
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				try {
+					dataIn.close();
+					s.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
 		
 	}
+		}
 }
